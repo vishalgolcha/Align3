71ce3ad1590c3cda39fbbc94967aa9b8ec4e6a6b
vishalgolcha <vishalgolcha@hotmail.com>
2017-09-30 15:15:16 +0530

    conditions added , readme added .

--- align3.py
+++ align3.py
@@ -19,34 +19,28 @@ class grid_create :
         
         self.gs= [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
         self.gc=0
-        self.cnt=0
-#         one for each column 
+        self.cnt=0 
         self.colm = [0,0,0,0]
-#         self.child_utils =[0,0,0,0]
         self.idx = -1
         self.idy = -1
         
+#   permanently sets the grid pos to a particular value 
     def perma_set(self,a,b,minmax):
         self.gs[a][b]= minmax*10 + minmax
-        self.colm[b] = self.colm[b]+1
-
-    def perma_reset(self,a,b):
-        self.gs[a][b]=0
-        self.colm[b] = a-1
-
+        self.colm[b] = self.colm[b]+1 
     def show_grid(self):
         print self.gs
         
+        
+#   helper function for terminate  function 
     def terminal(self,lx,ly,term):
         
         state= self.gs[lx][ly]
-    
         for i in term:
             flag=0
             for j in i:
-#                 next x;
+#               nx:  next x and ny: next y
                 nx=j[0]+lx 
-#                 next y;
                 ny=j[1]+ly 
                 
                 if valid(nx,ny)==True:
@@ -68,7 +62,9 @@ class grid_create :
                 continue ;
                 
         return False
-    
+
+#   resets the grid[a][b] to 1/2 depending on given minimax value in the current state of backtracking   
+
     def sett(self,a,b,minmax):
         
         if   minmax==1:
@@ -77,25 +73,39 @@ class grid_create :
             self.gs[a][b]=2;
             
 #         self.gc=self.gc+1;
-        
+    
+#   checks if the state is in termination or not 
     def terminate(self,lx,ly):
         check = False
         for i in options :
             check= check or self.terminal(lx,ly,i)
         return check 
         
-    
+#   resets the grid[a][b] to 0 in the current state of backtracking   
     def reset(self,a,b):
         self.gs[a][b]=0;
-#         self.gc=self.gc-1;
-
-
-#   the function should return utilities 
     
-    def recurse(self,alpha,beta,minmax,depth):         
+#   the function should return utilities 
+    def util(self,minmax,depth):
+        z=None
+        if   minmax== 0:
+            z=(-1.0/depth)*10;
+            
+        elif minmax== 1:
+            z=(1.0/depth)*10;            
+        
+        return z
+
+#     isab   : translates to whether the method is running in alpha beta or nor 
+#     palpha : parent's alpha 
+#     pbeta  : parent's beta
+
+    def recurse(self,palpha,pbeta,minmax,depth,isab):         
         
         ret_val = None
-        
+        alpha   = palpha
+        beta    = pbeta
+
         if   minmax==0: # second player choosing moves
             ret_val= 1000000007
             
@@ -107,11 +117,11 @@ class grid_create :
         
         
         for i,j in enumerate(self.colm):
-#             print self.colm[i],i,depth
-            # current position to chose 
+            
+#             current position to chose / mark 
             val1 = self.colm[i] 
             val2 = i
-            
+                        
             if j==4:
                 continue;
             
@@ -120,45 +130,22 @@ class grid_create :
             
             flag1=False 
             
-            if True :
+            self.sett(val1,val2,minmax);
+            if self.terminate(val1,val2)== True: 
+                flag1=True
+                z= self.util(minmax,depth)
+            self.reset(val1,val2);
+                
+            if flag1==False:     
                 self.colm[i]=self.colm[i]+1
                 self.sett(val1,val2,minmax);
-                
-                if self.terminate(val1,val2)== True: 
-                    flag1=True
-#                     print val1,val2,depth
-#                     check if the state is a terminating one 
-#                     return utility can be depth inverse  as of now go by the assignment
-#                     print(val1,val2)
-                    self.reset(val1,val2);
-                    self.colm[i]=self.colm[i]-1
-
-                    if   minmax== 0:
-#                          print depth,(-1.0/depth)*10;
-                          z=(-1.0/depth)*10;
-
-                    elif minmax== 1:
-#                         print depth,(1.0/depth)*10 ;
-                          z=(1.0/depth)*10;
-                else:
-                    self.reset(val1,val2);
-                    self.colm[i]=self.colm[i]-1
-
-                        
-            if flag1==False :     
-                self.colm[i]=self.colm[i]+1
-                self.sett(val1,val2,minmax);
-                
-                                        
-                z=self.recurse(alpha,beta,abs(minmax-1),depth+1)
-                
-                                            
+                z=self.recurse(alpha,beta,abs(minmax-1),depth+1,isab)
                 self.reset(val1,val2);
                 self.colm[i]=self.colm[i]-1
                 
             if   minmax==0:
                 ret_val= min(z,ret_val)
-
+                beta   = min(beta,z)
                 if depth==1 :
                     if ret_val == z :
                         self.idx= val1
@@ -166,9 +153,16 @@ class grid_create :
 
             elif minmax==1:
                 ret_val= max(z,ret_val)
-
+                alpha  = max(alpha,z)
                 if depth==1 :
                     if ret_val == z :
                         self.idx= val1
                         self.idy= val2
+                        
+#           this is where it gets pruned
+            if isab==1:
+                if alpha > beta :
+                    break
+                    
         return ret_val
+    
---
+++ readme.md
@@ -0,0 +1,11 @@
+##ALIGN-3 
+>A 4x4 board game is played by two players. The game is deterministic, turn taking, zero sumgame and can be named as align3. The board positions can be specified by the row numbers R1, R2, R3,and R4; and column numbers C1, C2, C3, C4. 
+
+### RULES
+>The row R1 is nearest to the base line as mentioned in the figure ,the two players are you (human, say H) and the intelligent agent (machine, say M ). In this game,player H gets 8 blue colored coins and player M gets 8 green colored coins. A player places a coin in anyone column at a vacant row position while it is necessary to ensure that rows near the baseline should not be left vacant before a coin is placed . 
+
+### HOW CAN YOU WIN THE GAME ?
+
+>The two players take turn one by one and strategically place their coins to win. The player who achieves the alignment of the three coins wins while the alignment can be horizontal, vertical or diagonal. 
+
+>A single alignment of three coins in anydirection by a player terminates the game. In this game, the coins never move once placed. If no playercan achieve an alignment of three coins and all 8 coins of both players are consumed, then the game is a draw. 
